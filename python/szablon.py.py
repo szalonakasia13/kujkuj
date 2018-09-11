5f4ed02f95b53645c42cdafac8815676787da4dd
szalonakasia13 <sieradzka857@gmail.com>
2018-09-11 12:22:08 +0200

    dodanie szablonu Pythona

---
+++ python/szablon.py.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py.py
+#  
+
+
+def main(args):
+    print("WitaaAAJJJJJ")
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
